2d278b814e494d3c859996621f0c08913aec8602
michaelheckmann <heckmannmichael@yahoo.de>
2025-05-19 21:42:10 +0200

    add license headers

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,11 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health Spezi Web Study Platform open-source project
+//
+// SPDX-FileCopyrightText: 2025 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
 import { defineConfig, devices } from "@playwright/test";
 
 /**
